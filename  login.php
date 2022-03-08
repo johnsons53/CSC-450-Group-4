7b4b33de87506bce401faa9f4bbd613a3dc54bd3
AhnellCSP <93556733+AhnellCSP@users.noreply.github.com>
2022-03-07 21:29:04 -0600

    Update  login.php
    Fixed comments, removed old versions of code.

---  login.php
+++  login.php
@@ -8,45 +8,41 @@
     /* login.php - Database Login information
       Spring 100 CSC 450 Capstone, Group 4
       Author: Lisa Ahnell
-      Date Written: 03/23/2022
-      Revised: n/a
+      Date Written: 03/03/2022
+      Revised: 03/07/2022 - modify method of storing data on page, update connection code'
+      removed testing notes and old functionality.
       */
 
     /* Replace these values for use on your local host, will update for site hosting.
      To use on other pages include this:
      <?php
-     require_once 'login.php';
-    // Create connection
-    $conn = new mysqli($db_hostname, $db_username, $db_password, $db_database);
+      $config = require ' login.php'; 
+      $db_hostname = $config['DB_HOSTNAME'];
+      $db_username = $config['DB_USERNAME'];
+      $db_password = $config['DB_PASSWORD'];
+      $db_database = $config['DB_DATABASE'];
+      // Create connection
+      $conn = new mysqli($db_hostname, $db_username, $db_password, $db_database);
 
-    // Check connection
-    if ($conn->connect_error) {
+      // Check connection
+      if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
-    }
+      }
 
-    echo "Connected successfully. <br />";
+      echo "Connected successfully. <br />";
 
-    $conn->close();
+      $conn->close();
 
-    echo "Connection closed.";
-     ?>
+      echo "Connection closed.";
+      ?>
     */
-
-    echo "This message is on the login page. <br />";
     return [
       "DB_HOSTNAME" => "localhost",
       "DB_DATABASE" => 'iep_portal',
       "DB_USERNAME" => 'root',
       'DB_PASSWORD' => 'root'
 
-    ];
-
-    //$db_hostname = 'localhost';
-    //$db_database = 'iep_portal';
-    //$db_username = 'root';
-    //$db_password = 'root';
-
-    
+    ];    
 ?>
 
 </body>
