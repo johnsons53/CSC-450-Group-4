fce0ba9e0bdbb54d7b54df2eafe6eb8e0a1cb277
AhnellCSP <93556733+AhnellCSP@users.noreply.github.com>
2022-03-15 09:45:41 -0700

    Query Testing and class definitions
    Testing queries, putting data from result object into arrays.
    
    Additionally, began writing PHP classes for the entities in our database, to store retrieved objects on pages.

--- databaseConnectionTestPage.php
+++ databaseConnectionTestPage.php
@@ -47,6 +47,7 @@
 
     // array to hold student user_id values
     $students = [];
+    $student_ids = [];
 
     // student_id and user_id values of a designated parent where parent_access is '1'
     $sql = "SELECT student_parent.student_id, student.user_id
@@ -63,6 +64,7 @@
             echo "student_id: " . $row["student_id"] . " ... user_id: " . $row["user_id"] . "<br />";
             // push user_id values into $students array
             $students[] = $row["user_id"];
+            $student_ids[] = $row["student_id"];
         }
     } else {
         echo "0 results <br />";    
@@ -100,6 +102,41 @@
     foreach ($names as $value) {
         echo "name: " . $value . "<br />";
     }
+
+    // Use student_id to select goals
+    $student_goals = [];
+    foreach ($student_ids as $value) {
+        // get the goals for each student in $students
+        $sql = "SELECT *
+                FROM goal
+                WHERE student_id=" . $value;
+
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            echo "Goals Found: <br />";
+            while ($row = $result->fetch_assoc()) {
+                echo "goal_id: " . $row["goal_id"] . " student_id: " . $row["student_id"] . " goal_label: " . $row["goal_label"];
+                echo "<br />";
+
+                // push goal_id to $student_goals array
+                $student_goals[] = $row["goal_id"];
+
+            }
+        } else {
+            echo "0 results for goals <br />"; 
+        }
+    }
+    // display contents of $student_goals
+    echo "List of all student goals visible to example parent: <br />";
+    foreach ($student_goals as $value) {
+        echo "goal_id: " . $value . "<br />";
+    }
+
+    // select objective by goal_id
+    
+
+    // select report by objective_id
 
     // close connection
     $conn->close();
