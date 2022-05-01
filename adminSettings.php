37acf4d32ade4c81251b8aaaebff67768ac4440d
Andy <yanga28@csp.edu>
2022-05-01 09:44:56 -0500

    Added Check to iepLogin.php
    - Added check for if user is logged in, going back to login page will redirect to dashboard page.
    
    - Also placed style.css on the bottom in adminSettings.php for better display in admin dashboard.

--- adminSettings.php
+++ adminSettings.php
@@ -48,11 +48,11 @@
       <head>
           <title>IEP Admin Settings</title>
           <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-          <link rel="stylesheet" type="text/css" href="style.css">
           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
           <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+          <link rel="stylesheet" type="text/css" href="style.css">
+          
       </head>
 
       <body>
